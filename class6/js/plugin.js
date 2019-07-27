0d312daf24b762cbd683dcc096b617ae453b71c0
mrzhang638993 <1446719386@qq.com>
2019-07-27 16:35:58 +0800

    commit  files

---
+++ class6/js/plugin.js
@@ -0,0 +1,21 @@
+(function(global,factory){
+
+    global.VueRouter=factory();
+
+
+})(this,function(){
+    // 对应的这一部分的代码对应的是插件的核心代码逻辑实现，主题的逻辑需要书写到这里面的。
+
+    var  VueRouter=function(){}
+
+    VueRouter.prototype={
+        push:function(){
+
+        },
+
+        add:function(){
+
+        }
+    }
+    return "路由方法";
+});  //  自执行函数
---
+++ my-project/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+/*
+   对应的./router 是对应的引入相关的文件夹下面的内容的。
+*/
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
